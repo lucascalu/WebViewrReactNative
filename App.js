f385e180f89ec86e5cf25c0b7c26da6a48bc71ca
Lucas Schiavon Calu <54896910+lucascalu@users.noreply.github.com>
2023-10-17 15:16:19 -0300

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ const HTMLViewer = () => {
     <WebView
       source={{ html: '<html><body><h1>Meu Arquivo HTML</h1></body></html>' }}
       // Substitua a string acima pelo conteúdo HTML que você deseja exibir.
+  //caso troque html por url ele exibe uma pagina web.
     />
   );
 };
